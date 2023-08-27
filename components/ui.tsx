b42bdaa4ffb57fdedeb0ecd3204b208cb0d66a4e
Matthew Dean <37552721+matttdean@users.noreply.github.com>
2023-08-27 01:19:32 -0400

    add conditional check if user is not null on ui component

--- components/ui.tsx
+++ components/ui.tsx
@@ -14,7 +14,7 @@ export default function UI() {
     
     <div className='w-full flex justify-evenly items-center text-4xl absolute py-5 z-50 bg-black/30 top-0 backdrop-blur-sm'>
         <div className='flex flex-col text-white/90'><h2 className='text-lg'>Score</h2>{score}</div>
-        <div className='bg-white w-auto h-auto flex flex-col justify-center items-center p-6 text-center rounded-full'><div>{target.sound}</div><div className='text-xs'>{user.name}</div></div>
+        <div className='bg-white w-auto h-auto flex flex-col justify-center items-center p-6 text-center rounded-full'><div>{target.sound}</div><div className='text-xs'>{user && user.name}</div></div>
         <div className='flex flex-col  text-white/90'><h2 className='text-lg'>Health</h2>{health}</div>
     </div>
   )
